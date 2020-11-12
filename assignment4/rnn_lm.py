cc6c6885546a345fa031b57e2789b4a91dbc8592
Liping Liu <liping.liulp@gmail.com>
2020-11-12 10:15:19 -0500

    update the evaluation method

--- assignment4/rnn_lm.py
+++ assignment4/rnn_lm.py
@@ -1,3 +1,21 @@
 # You are supposed to put your own RNN model here
 
+import tensorflow as tf
+
+def masked_lm_loss(labels, logits):
+
+    # get flags for actual chars. Padding chars are -1
+    flag_chars = tf.cast(labels >= 0, tf.float32)
+
+    # turn labels to one-hot vectors. -1 will generate a zero vector
+    labels_onehot = tf.one_hot(labels, depth=logits.shape[-1])
+
+    # compute CE loss against logits
+    loss_mat = tf.keras.losses.categorical_crossentropy(labels_onehot, logits, from_logits=True)
+
+    # get the per-char-loss for each sentence
+    loss_val = tf.reduce_sum(loss_mat * flag_chars, axis=1) / tf.reduce_sum(flag_chars, axis=1)
+
+    return loss_val
+
 
